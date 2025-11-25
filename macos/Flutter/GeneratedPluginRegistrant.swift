b0a26cb00e13ad5a921dff0dcd993e5bd474cf9e
Thiago Loureiro <thiagoguaru@me.com>
2025-11-25 18:49:41 -0500

    Update version to 1.0.18, upgrade dependencies including flutter_dotenv, package_info_plus, and device_info_plus, and adjust iOS and Android build configurations to match the new version.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import flutter_new_badger
 import flutter_secure_storage_macos
 import mobile_scanner
 import package_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
 import url_launcher_macos
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
